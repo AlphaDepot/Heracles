aabcdd9eaa4f2121a4ec84c3a703a650398cd2f9
lstsystems <leyter@leytersuarez.com>
2024-05-24 18:08:05 -0400

    Updated string constants in TestDomainResponse class
    Changed the nullable string constants `Success`, `NotFound`, and `BadRequest` in the `TestDomainResponse` class to non-nullable. This ensures that these constants will always hold a string value and cannot be null.

--- Tests/Heracles.TestUtilities/Helpers/TestDomainResponse.cs
+++ Tests/Heracles.TestUtilities/Helpers/TestDomainResponse.cs
@@ -2,9 +2,9 @@ namespace Heracles.TestUtilities.Helpers;
 
 public static class TestDomainResponse
 {
-    public const string? Success = "Success";
-    public const string? NotFound = "NotFound";
+    public const string Success = "Success";
+    public const string NotFound = "NotFound";
     public const string Duplicate = "Duplicate";
-    public const string? BadRequest = "BadRequest";
+    public const string BadRequest = "BadRequest";
     
 }
